b7b98022b6052bf9b8b1f793dfb881234712bf7c
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2021-02-25 09:38:48 +0100

    Add shop page

---
+++ pages/shop/[slug].js
@@ -0,0 +1,24 @@
+export default function ProductDetails({ product }) {
+  return <div>{product}</div>;
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [
+      { params: { slug: 'first' } },
+      { params: { slug: 'second' } },
+      { params: { slug: 'third' } },
+    ],
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(ctx) {
+  console.log('getStaticProps', ctx);
+
+  return {
+    props: {
+      product: ctx.params.slug,
+    },
+  };
+}
